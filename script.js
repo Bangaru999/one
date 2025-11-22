328bc6b3b6b4799ee9f585324e60f4111324db86
Bangaru999 <98631998+Bangaru999@users.noreply.github.com>
2025-11-22 13:57:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,4 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
   alert('Thank you for your message! We will get back to you soon.');
   this.reset();
 });
+//this is iam testing for the conceopt called webhook, thanks!
